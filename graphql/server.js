ff5f8ffacc7636dd8b683b70e7f09c88c805b79f
Justin <justindado@yahoo.com>
2016-12-10 14:36:15 +1300

    added graphql server

---
+++ graphql/server.js
@@ -0,0 +1,29 @@
+import express from 'express'
+
+import { apolloExpress, grapiqlExpress } from 'apollo-server'
+import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools'
+import bodyParser from 'body-parser'
+
+const GRAPHQL_PORT = 2876
+
+const graphQLServer = express()
+
+const executableSchema = makeExecutableSchema({
+  typeDefs: Schema,
+  resolvers: Resolvers,
+  allowUndefinedInResolve: false,
+  printErrors: true
+})
+
+graphQLServer.use('/graphql', bodyParse.json(), apolloExpress({
+  schema: executableSchema,
+  context: {}
+}))
+
+graphQLServer.use('/graphiql', graphiqlExpress({
+  endpointURL: '/graphql'
+}))
+
+graphQLServer.listen(GRAPHQL_PORT, () => console.log(
+  `GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}/graphql`
+))
