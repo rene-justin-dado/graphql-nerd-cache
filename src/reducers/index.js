8db2f7d34276d60fb28550b0bdc7b564b028e954
Justin <justindado@yahoo.com>
2016-12-12 21:06:48 +1300

    combined apollo client reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux'
+import ApolloClient from 'apollo-client'
 
 import words from './words'
 
+const client = new ApolloClient()
 export default combineReducers({
-  words
+  words,
+  apollo: client.reducer()
 })
-
