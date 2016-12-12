8a901b70c39e837c7ff175c6ecaa1a3b8183061f
Justin <justindado@yahoo.com>
2016-12-12 21:34:38 +1300

    added some descriptions to the schema

--- graphql/data/schema.js
+++ graphql/data/schema.js
@@ -1,9 +1,12 @@
 const typeDefinitions = `
+# An Epic hero from the Starcraft universe.
 type Hero {
   id: Int!
+  # A hero's name or alias.
   name: String!
   race: [Race]
 }
+# A combat unit from the tumultuous Koprulu sector.
 type Unit {
   id: Int!
   name: String!
@@ -12,16 +15,18 @@ type Unit {
   armor: String
   abilities: [Abilities]
 }
+# A unit's or hero's special abilities in-game.
 type Abilities {
   id: Int!
   name: String!
 }
+# Terran, Protoss or Zerg.
 type Race {
   name: String!
   race: String
 }
 
-# the schema allows the following query:
+# You can search Heroes and Units:
 type Query {
   hero(name: String): Hero
   unit(name: String): Unit
