7adee4b12b80375df6ccf8536f0c342253ab1105
Justin <justindado@yahoo.com>
2016-12-12 18:47:16 +1300

    added sequelize definitions for hero and unit

--- graphql/data/connectors.js
+++ graphql/data/connectors.js
@@ -1,6 +1,17 @@
 import Sequelize from 'sequelize'
 
-const db = new Sequelize('heroes', null, null, {
+const db = new Sequelize('starcraftData', null, null, {
   dialect: 'sqlite',
   storage: './starcraft.db'
+})
+
+const HeroModel = db.define('hero', {
+  name: { type: Sequelize.STRING }
+})
+
+const UnitModel = db.define('unit', {
+  name: { type: Sequelize.STRING },
+  race: { type: Sequelize.STRING },
+  weapon: { type: Sequelize.STRING },
+  armor: { type: Sequelize.STRING }
 })
