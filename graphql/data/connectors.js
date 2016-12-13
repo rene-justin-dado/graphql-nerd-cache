eb9d460d439ecc6e61b18ddbc94017e50b060b55
Justin <justindado@yahoo.com>
2016-12-13 22:57:26 +1300

    freezing table names

--- graphql/data/connectors.js
+++ graphql/data/connectors.js
@@ -1,12 +1,14 @@
 import Sequelize from 'sequelize'
 
-const db = new Sequelize('starcraftData', null, null, {
+const db = new Sequelize('starcraft', null, null, {
   dialect: 'sqlite',
-  storage: './starcraft.db'
+  storage: './starcraft.sqlite'
 })
 
 const HeroModel = db.define('hero', {
   name: { type: Sequelize.STRING }
+}, {
+  freezeTableName: true
 })
 
 const UnitModel = db.define('unit', {
@@ -14,16 +16,34 @@ const UnitModel = db.define('unit', {
   faction: { type: Sequelize.STRING },
   weapon: { type: Sequelize.STRING },
   armor: { type: Sequelize.STRING }
+}, {
+  freezeTableName: true
 })
 
 const RaceModel = db.define('race', {
   name: { type: Sequelize.STRING }
+}, {
+  freezeTableName: true
 })
 
 HeroModel.hasOne(RaceModel)
 UnitModel.hasOne(RaceModel)
 RaceModel.belongsToMany(HeroModel, {through: 'HeroRace'})
 RaceModel.belongsToMany(UnitModel, {through: 'UnitRace'})
+
+db.sync({force:true, match: /starcraft$/})
+  .then(() => {
+    () => HeroModel.create({
+      id: 1,
+      name: "Zeratul"
+    })
+  })
+  .then(() => {
+    () => HeroModel.create({
+      id: 2,
+      name: "James Raynor"
+    })
+  })
 
 const Hero = db.models.hero
 const Unit = db.models.unit
