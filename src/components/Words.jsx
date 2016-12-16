aa69114fabe97a68968ab8fa29c2690a24e78842
Justin <justindado@yahoo.com>
2016-12-16 16:32:48 +1300

    moved graphql request to its own component

--- src/components/Words.jsx
+++ src/components/Words.jsx
@@ -1,6 +1,4 @@
 import React, {PropTypes} from 'react'
-import {graphql} from 'react-apollo'
-import gql from 'graphql-tag'
 
 import Word from './Word'
 
@@ -21,21 +19,7 @@ Words.propTypes = {
       id: PropTypes.number.isRequired,
       word: PropTypes.string.isRequired
     }).isRequired
-  ).isRequired,
-  data: PropTypes.shape({
-    hero: PropTypes.object
-  }).isRequired
+  ).isRequired
 }
 
-const Zeratul = gql`
-query Zeratul {
-  hero(name:"Zeratul") {
-    id
-    name
-    race
-  }
-}
-`
-
-const WordsWithData = graphql(Zeratul)(Words)
-export default WordsWithData
+export default Words
