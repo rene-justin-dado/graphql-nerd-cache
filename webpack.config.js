ad8449101ce53c4dc211a1611ee2e3aea16aadbc
Justin <justindado@yahoo.com>
2016-12-17 22:53:33 +1300

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,16 @@
-const webpack = require('webpack')
 module.exports = {
-  entry: [
-    'webpack-dev-server/client?http://127.0.0.1:1234',
-    'webpack/hot/only-dev-server',
-    './src/index.js'
-  ],
-  output: 'bundle.js',
+  entry: './src/index.js',
+  output: {
+    path: `${__dirname}/src/public`,
+    filename: 'bundle.js'
+  },
   module: {
     loaders: [
-      { loader: 'babel-loader', test: /\.jsx?$/, exclude: 'node_modules' }
+      { loader: 'babel-loader?cacheDirectory', test: /\.jsx?$/, exclude: 'node_modules' }
     ]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()],
   devtool: 'source-map'
 }
