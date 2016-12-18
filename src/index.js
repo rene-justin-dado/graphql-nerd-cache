bdc91494adb27116100643cf756b33d66f8c3bee
Justin <justindado@yahoo.com>
2016-12-18 21:40:56 +1300

    attempting to connect client to graphql server

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 import React from 'react'
 import {render} from 'react-dom'
-import {Provider} from 'react-redux'
 import {createStore, applyMiddleware, compose} from 'redux'
 
-import ApolloClient from 'apollo-client'
+import ApolloClient, {createNetworkInterface} from 'apollo-client'
 import {ApolloProvider} from 'react-apollo'
 
 import reducers from './reducers'
 
 import App from './components/App'
 
-const client = new ApolloClient()
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface({uri: 'http://localhost:3000/graphiql/'})
+})
 let store = createStore(
   reducers,
   {},
   compose(
     applyMiddleware(client.middleware()),
-    window.devToolsExtension ? window.devToolsExtension() : f => f,
+    window.devToolsExtension ? window.devToolsExtension() : f => f
   )
 )
 
