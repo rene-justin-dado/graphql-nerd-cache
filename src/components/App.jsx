bc27468edab792973777685b1bc84545225c9cd6
Justin <justindado@yahoo.com>
2017-01-31 22:18:31 +1300

    removed unnecessary components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,11 @@
 import React from 'react'
 
-import AddWord from './AddWord'
-import WordsContainer from '../containers/WordsContainer'
 import StarcraftHeroesContainer from '../containers/StarcraftHeroesContainer'
 
 export default React.createClass({
   render () {
     return (
       <div className='app-container'>
-        <WordsContainer />
-        <AddWord />
-        <br/>
-        <br/>
         <StarcraftHeroesContainer />
       </div>
     )
