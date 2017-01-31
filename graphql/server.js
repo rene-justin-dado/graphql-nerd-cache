4cce22e94a697e2b4f191c03ffe64a25b5b2af38
Justin <justindado@yahoo.com>
2017-01-31 22:06:46 +1300

    applying cors to graphql endpoint

--- graphql/server.js
+++ graphql/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import {apolloExpress, graphiqlExpress} from 'apollo-server'
 import {makeExecutableSchema} from 'graphql-tools'
 import bodyParser from 'body-parser'
+import cors from 'cors'
 import Schema from './data/schema'
 import Resolvers from './data/resolvers'
 
@@ -17,6 +18,7 @@ const executableSchema = makeExecutableSchema({
 })
 
 graphQLServer.use(express.static(`${__dirname}/../public`))
+graphQLServer.use(cors())
 
 graphQLServer.use('/graphql', bodyParser.json(), apolloExpress({
   schema: executableSchema,
