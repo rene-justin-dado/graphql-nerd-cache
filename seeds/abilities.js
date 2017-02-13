93061e31ae8c8d1714ea31e3655e4c33ccf525a1
Justin <justindado@yahoo.com>
2017-02-13 20:02:54 +1300

    updated seeds

--- seeds/abilities.js
+++ seeds/abilities.js
@@ -6,13 +6,39 @@ exports.seed = function(knex, Promise) {
       return Promise.all([
         // Inserts seed entries
         knex('abilities').insert({
-          ability: 'Build'
+          ability: 'Build',
+          hero_id: null,
+          unit_id: 888881
         }),
         knex('abilities').insert({
-          ability: 'Repair'
+          ability: 'Build',
+          hero_id: null,
+          unit_id: 888882
         }),
         knex('abilities').insert({
-          ability: 'Gather'
+          ability: 'Build',
+          hero_id: null,
+          unit_id: 888883
+        }),
+        knex('abilities').insert({
+          ability: 'Repair',
+          hero_id: null,
+          unit_id: 888881
+        }),
+        knex('abilities').insert({
+          ability: 'Gather',
+          hero_id: null,
+          unit_id: 888881
+        }),
+        knex('abilities').insert({
+          ability: 'Gather',
+          hero_id: null,
+          unit_id: 888882
+        }),
+        knex('abilities').insert({
+          ability: 'Gather',
+          hero_id: null,
+          unit_id: 888883
         })
       ]);
     });
--- seeds/unit.js
+++ seeds/unit.js
@@ -6,36 +6,42 @@ exports.seed = function(knex, Promise) {
       return Promise.all([
         // Inserts seed entries
         knex('unit').insert({
+          id: 888881,
           name: 'SCV',
           race: 'Terran',
           weapon: 'Fusion Cutter',
           armor: 'Light Neosteel Vehicle Plating'
         }),
         knex('unit').insert({
+          id: 888882,
           name: 'Probe',
           race: 'Protoss',
           weapon: 'Particle Beam',
           armor: 'Plasma Shields/Protoss Armor'
         }),
         knex('unit').insert({
+          id: 888883,
           name: 'Drone',
           race: 'Zerg',
           weapon: 'Claws',
           armor: 'Carapace'
         }),
         knex('unit').insert({
+          id: 888884,
           name: 'Marine',
           race: 'Terran',
           weapon: 'C-14 Impaler Gauss Rifle',
           armor: 'CMC 300/400 Combat Suit'
         }),
         knex('unit').insert({
+          id: 888885,
           name: 'Zealot',
           race: 'Protoss',
           weapon: 'Psi Blades',
           armor: 'Plasma Shields/Protoss Armor'
         }),
         knex('unit').insert({
+          id: 888886,
           name: 'Zergling',
           race: 'Zerg',
           weapon: 'Claws, Fangs',
