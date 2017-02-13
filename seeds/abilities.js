fd4eea0988747f19ee836ac32add4787f87e31c1
Justin <justindado@yahoo.com>
2017-02-13 21:29:36 +1300

    updated seeds

--- seeds/abilities.js
+++ seeds/abilities.js
@@ -7,39 +7,32 @@ exports.seed = function(knex, Promise) {
         // Inserts seed entries
         knex('abilities').insert({
           ability: 'Build',
-          hero_id: null,
           unit_id: 888881
         }),
         knex('abilities').insert({
           ability: 'Build',
-          hero_id: null,
           unit_id: 888882
         }),
         knex('abilities').insert({
           ability: 'Build',
-          hero_id: null,
           unit_id: 888883
         }),
         knex('abilities').insert({
           ability: 'Repair',
-          hero_id: null,
           unit_id: 888881
         }),
         knex('abilities').insert({
           ability: 'Gather',
-          hero_id: null,
           unit_id: 888881
         }),
         knex('abilities').insert({
           ability: 'Gather',
-          hero_id: null,
           unit_id: 888882
         }),
         knex('abilities').insert({
           ability: 'Gather',
-          hero_id: null,
           unit_id: 888883
         })
-      ]);
-    });
-};
+      ])
+    })
+}
--- seeds/hero.js
+++ seeds/hero.js
@@ -6,26 +6,32 @@ exports.seed = function(knex, Promise) {
       return Promise.all([
         // Inserts seed entries
         knex('hero').insert({
+          id: 999991,
           name: 'James Raynor',
           race: 'Terran'
         }),
         knex('hero').insert({
+          id: 999992,
           name: 'Matt Horner',
           race: 'Terran'
         }),
         knex('hero').insert({
+          id: 999993,
           name: 'Alarak',
           race: 'Protoss'
         }),
         knex('hero').insert({
+          id: 999994,
           name: 'Vorazun',
           race: 'Protoss'
         }),
         knex('hero').insert({
+          id: 999995,
           name: 'Dehaka',
           race: 'Zerg'
         }),
         knex('hero').insert({
+          id: 999996,
           name: 'Kerrigan',
           race: 'Zerg'
         })
