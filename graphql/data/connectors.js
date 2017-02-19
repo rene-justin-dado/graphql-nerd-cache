489a051583e596ea2970e3cdfe5fcbe28cc6419f
Justin <justindado@yahoo.com>
2017-02-19 21:32:26 +1300

    added separate queries for units with and without abilities

--- graphql/data/connectors.js
+++ graphql/data/connectors.js
@@ -41,3 +41,46 @@ export const Unit = (name) => (
     .then(namedUnit => namedUnit)
     .catch(err => console.error(err))
   )
+
+export const unitHasNoAbilities = () => (
+  knex('unit')
+    .leftJoin('abilities', 'unit.id', '=', 'unit_id')
+    .select('unit.id', 'name', 'race', 'weapon', 'armor', 'hp', 'shields')
+    .whereNull('abilities.id')
+    .then(unitsArray => unitsArray.map((unit) => Object.assign({abilities: null}, unit)))
+    .catch(err => console.error(err))
+)
+
+export const unitHasAbilities = () => (
+  knex('unit')
+    .leftJoin('abilities', 'unit.id', '=', 'unit_id')
+    .whereNotNull('abilities.ability')
+    .select('unit.id', 'name', 'race', 'weapon', 'armor', 'hp', 'shields', 'abilities.id AS abilityId', 'ability')
+    .groupBy('unit.id', 'abilityId')
+    .orderBy('unit.id')
+    .then(unitsArray => {
+      let abilities = []
+      let unitAbilities = []
+      return unitsArray.reduce((prevUnit, currUnit, i, arr) => {
+        if (prevUnit.id === currUnit.id) {
+          abilities.push({id: prevUnit.abilityId, ability: prevUnit.ability})
+        }
+        if (prevUnit.id !== currUnit.id) {
+          abilities.push({id: prevUnit.abilityId, ability: prevUnit.ability})
+          unitAbilities.push(Object.assign({abilities: abilities}, prevUnit))
+          abilities = []
+        }
+        if (i === arr.length - 1) {
+          abilities.push({id: currUnit.abilityId, ability: currUnit.ability})
+          unitAbilities.push(Object.assign({abilities: abilities}, currUnit))
+          return unitAbilities
+        }
+        return currUnit
+      })
+    })
+    .then(data => data)
+    .catch(err => console.error(err))
+)
+
+export const Units = () => {
+}
