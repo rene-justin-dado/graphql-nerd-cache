8fed635339ac53ba66b137b23a9348d1666a440d
Justin <justindado@yahoo.com>
2017-02-20 20:27:52 +1300

    presenting hero values in pie chart

--- src/components/StarcraftHeroesList.jsx
+++ src/components/StarcraftHeroesList.jsx
@@ -6,38 +6,72 @@ export default React.createClass({
     data: React.PropTypes.object.isRequired
   },
   d3 () {
-    let heroValues = []
+    // data
     const heroes = this.props.data.heroes
-    heroes.forEach((val) => heroValues.push(val.charValue))
-    const height = 200,
-      width = 1000,
-      barWidth = 80
+    // init values
+    const height = 360,
+      width = 360,
+      radius = Math.min(height, width) / 2,
+      color = d3.scaleOrdinal(d3.schemeCategory20b)
 
-    const y = d3.scaleLinear()
-                .domain([0, d3.max(heroValues)])
-                .range([200, 0])
-    const heroesChart = d3.select('.heroesChart')
-                    .attr('width', width)
-                    .attr('height', height)
-    const bar = heroesChart.selectAll('g')
-                       .data(heroes)
-                     .enter().append('g')
-                       .attr('transform', (d, i) => `translate(${i * barWidth}, 0)`)
-    bar.append("rect")
-     .attr('y', (d) => y(d.charValue))
-     .attr("width", barWidth - 1)
-     .attr("height", (d) => height - y(d.charValue))
+    const arc = d3.arc()
+      .innerRadius(0)
+      .outerRadius(radius)
+    const labelArc = d3.arc()
+      .outerRadius(radius - 80)
+      .innerRadius(radius - 80)
+    const pie = d3.pie()
+      .value((d) => d.charValue)
+      .sort(null)
 
-    bar.append("text")
-       .attr("x", barWidth / 6)
-       .attr("y",  (d) => y(d.charValue) + 5)
-       .attr("dy", ".75em")
-       .text((d) => d.name)
+    const svg = d3.select('.heroesChart')
+                  .append('svg')
+                  .attr('width', width)
+                  .attr('height', height)
+                  .append('g')
+                  .attr('transform', `translate(${width/2}, ${height/2})`)
+                  .selectAll('.slice')
+                  .data(pie(heroes))
+                    .enter().append('g')
+                  .attr('class', 'slice')
+
+    const path = svg.append('path')
+                    .attr('d', arc)
+                    .attr('fill', (d) => color(d.data.name))
+    const text = svg.append('text')
+                    .attr('transform', (d) => `translate(${labelArc.centroid(d)})`)
+                    .attr('textLength', 80)
+                    .attr('lengthAdjust', 'spacing')
+                    .attr('dy', '.75em')
+                    .text(d => d.data.name)
+    // const y = d3.scaleLinear()
+    //             .domain([0, d3.max(heroes.map((hero) => hero.charValue))])
+    //             .range([200, 0])
+    // const heroesChart = d3.select('.heroesChart')
+    //                 .attr('width', width)
+    //                 .attr('height', height)
+    // const bar = heroesChart.selectAll('g')
+    //                    .data(heroes)
+    //                  .enter().append('g')
+    //                    .attr('transform', (d, i) => `translate(${i * barWidth}, 0)`)
+    // bar.append("rect")
+    //  .attr('y', (d) => y(d.charValue))
+    //  .attr("width", barWidth - 1)
+    //  .attr("height", (d) => height - y(d.charValue))
+    //
+    // bar.append("text")
+    //    .attr("x", barWidth / 6)
+    //    .attr("y",  (d) => y(d.charValue) + 5)
+    //    .attr("dy", ".75em")
+    //    .text((d) => d.name)
   },
   render () {
     const loading = this.props.data.loading
     return (
-      <svg className="heroesChart">{loading ? <h1>Loading. Please wait...</h1> : this.d3()}</svg>
+      <div className="heroesChart">
+        {loading ? <h1>Loading. Please wait...</h1> : <h3>Hero Value to Narrative</h3>}
+        {loading ? <h1>Loading. Please wait...</h1> : this.d3()}
+      </div>
     )
   }
 })
--- src/containers/StarcraftHeroesContainer.jsx
+++ src/containers/StarcraftHeroesContainer.jsx
@@ -3,20 +3,6 @@ import gql from 'graphql-tag'
 
 import StarcraftHeroesList from '../components/StarcraftHeroesList'
 
-const Generic = gql`
-  query Hero {
-    hero(name:"Kerrigan") {
-      name
-      race
-    }
-    unit(name:"SCV") {
-      name
-      race
-      weapon
-      armor
-    }
-  }
-`
 const Heroes = gql`
   query Heroes {
     heroes {
