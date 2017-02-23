e6cb48ecd55876b87c9e470d35832b2821acf845
Justin <justindado@yahoo.com>
2017-02-24 12:59:45 +1300

    removed unused reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,7 @@
 import {combineReducers} from 'redux'
 import ApolloClient from 'apollo-client'
 
-import words from './words'
-
 const client = new ApolloClient()
 export default combineReducers({
-  words,
   apollo: client.reducer()
 })
