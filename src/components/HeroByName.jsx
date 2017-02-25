15f464a02b07f28be0e908c33223fcb47f622191
Justin <justindado@yahoo.com>
2017-02-25 18:52:18 +1300

    renamed Results component

--- src/components/HeroByName.jsx
+++ src/components/HeroByName.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import StarcraftHero from './StarcraftHero'
+import StarcraftHeroFactions from './StarcraftHeroFactions'
 
 export default React.createClass({
   props: {
@@ -7,9 +7,7 @@ export default React.createClass({
   },
   render () {
     return (this.props.data.loading ? <h3>Loading. Please wait...</h3> :
-      this.props.data.hero ?
-        <StarcraftHero data={this.props.data} /> :
-        <h1>Nope</h1>
+        <StarcraftHeroFactions data={this.props.data} />
     )
   }
 })
